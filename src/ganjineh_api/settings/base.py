aaadbecb98b37cd1d93b3b318f438c9a8d372463
Amir Bapiri <amirbapiri@gmail.com>
2024-05-18 18:31:10 +0330

    Start subscriptions app

--- src/ganjineh_api/settings/base.py
+++ src/ganjineh_api/settings/base.py
@@ -46,6 +46,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "core_apps.users",
     "core_apps.profiles",
+    "core_apps.subscriptions",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
