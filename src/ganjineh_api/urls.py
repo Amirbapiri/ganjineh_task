c367d39813c0ba05ffee72f93da1a3f76f470fe2
Amir Bapiri <amirbapiri@gmail.com>
2024-05-18 16:53:36 +0330

    Create api versioning structure

--- src/ganjineh_api/urls.py
+++ src/ganjineh_api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    path("api/v1/", include(("core_apps.api_v1", "api_v1"), namespace="api_v1")),
 ]
