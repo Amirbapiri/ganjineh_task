5b211c1cb6a81410648b141bed8a6941230df275
Amir Bapiri <amirbapiri@gmail.com>
2024-05-18 20:44:48 +0330

    Create initial models for subscription system

--- src/core_apps/subscriptions/models.py
+++ src/core_apps/subscriptions/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class SubscriptionPlan(models.Model):
+    name = models.CharField(max_length=100)
+    daily_credits = models.PositiveIntegerField()
+    token_access = models.BooleanField(default=False)
+    credit_increase = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class UserSubscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_subscriptions",
+    )
+    plan = models.ForeignKey(
+        SubscriptionPlan,
+        on_delete=models.CASCADE,
+        related_name="user_subscriptions",
+    )
+    credits_remaining = models.PositiveIntegerField()
+    expires_at = models.DateField()
+    is_approved = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.plan.name}"
