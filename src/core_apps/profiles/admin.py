b3cd7c38405c5f805df6994dd827f04e9f4186d4
Amir Bapiri <amirbapiri@gmail.com>
2024-05-18 18:06:47 +0330

    Add Profile model to the admin site

--- src/core_apps/profiles/admin.py
+++ src/core_apps/profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Profile
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "user_type", "credits_remaining")
+    search_fields = ("user__email", "user_type")
